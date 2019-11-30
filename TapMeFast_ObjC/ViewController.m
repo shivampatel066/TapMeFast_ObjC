a1fec3952a9d3f6d1766d6483dddf6bf50613fe8
Shivam Patel <shivampatel066@gmail.com>
2019-11-30 13:00:07 +0530

    Initial Commit

---
+++ TapMeFast_ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TapMeFast_ObjC
+//
+//  Created by Shivam Patel on 30/11/19.
+//  Copyright © 2019 Shivam Patel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ TapMeFast_ObjCTests/TapMeFast_ObjCTests.m
@@ -0,0 +1,37 @@
+//
+//  TapMeFast_ObjCTests.m
+//  TapMeFast_ObjCTests
+//
+//  Created by Shivam Patel on 30/11/19.
+//  Copyright © 2019 Shivam Patel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TapMeFast_ObjCTests : XCTestCase
+
+@end
+
+@implementation TapMeFast_ObjCTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
