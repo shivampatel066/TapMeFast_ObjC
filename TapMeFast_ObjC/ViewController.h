85c5e3f4e825347fe226313a6236fa03aaad950d
Shivam Patel <shivampatel066@gmail.com>
2019-11-30 14:52:07 +0530

    TapMeFast
    Tap Quickly & Score A Grade

--- TapMeFast_ObjC/ViewController.h
+++ TapMeFast_ObjC/ViewController.h
@@ -8,7 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    NSTimer *timer;
+    int scoreInt;
+    int timerInt;
+    int gameMode;
+    
+}
+
+
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+
+- (IBAction)startGame:(id)sender;
+
 
 
 @end
--- TapMeFast_ObjC/ViewController.m
+++ TapMeFast_ObjC/ViewController.m
@@ -16,8 +16,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"%i",timerInt];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+    scoreInt = 0;
+    timerInt = 10;
+    gameMode = 0;
+    
 }
 
+//If gameMode = 1 // Game is Running
+//If gameMode = 0 // Game is Stopped
+- (IBAction)startGame:(id)sender {
+    
+    if (timerInt == 10) {
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.25;
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startTimer) userInfo:nil repeats:true];
+        
+    }
+    
+    if (gameMode == 1) {
+        scoreInt += 1;
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+    } else {
+        timerInt = 10;
+        scoreInt = 0;
+        self.timeLabel.text = [NSString stringWithFormat:@"%i",timerInt];
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+        
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    
+}
 
+-(void)startTimer {
+    
+    timerInt -= 1;
+    gameMode = 1;
+    [self.startButton setTitle:@"Tap" forState:UIControlStateNormal];
+    self.startButton.alpha = 1.0;
+    self.startButton.enabled = YES;
+    self.timeLabel.text = [NSString stringWithFormat:@"%i",timerInt];
+    
+    
+    if (timerInt == 0) {
+        [timer invalidate];
+        
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.25;
+        [self.startButton setTitle:@"Restart" forState:UIControlStateNormal];
+        timer = [NSTimer scheduledTimerWithTimeInterval: 3.0 target:self selector:@selector(restart) userInfo:nil repeats:true];
+        
+    }
+}
+
+-(void)restart {
+   
+    [timer invalidate];
+    self.startButton.enabled = YES;
+    self.startButton.alpha = 1.0;
+    gameMode = 0;
+}
 @end
